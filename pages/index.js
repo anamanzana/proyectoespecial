a4882e7d749945d984e6666b5a1bd82a944dc8bb
anamanzana <amahns@uc.cl>
2022-07-13 20:19:34 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>¡Bienvenid@ a IIC2513!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
